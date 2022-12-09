f49bafe175d03e8dbbe44a83a90444e070f75c41
Ramiro Mota <rockett117@gmail.com>
2022-12-09 17:17:26 -0600

    Parte 4

--- ecomerce-react/src/components/product/Main-product.jsx
+++ ecomerce-react/src/components/product/Main-product.jsx
@@ -1,11 +1,10 @@
 import DetailsProduct from "@/components/product/DetailsProduct"
+import SlideProduct from "@/components/product/SlideProduct"
 
 const MainProduct = () => {
     return (
         <main className="grid grid-cols-1 md:grid-cols-2 gap-8">
-            <section>
-                Galeria de imagenes
-            </section>
+            <SlideProduct />
             <DetailsProduct />
         </main>
     )
--- ecomerce-react/src/components/product/SlideProduct.jsx
+++ ecomerce-react/src/components/product/SlideProduct.jsx
@@ -1,7 +1,31 @@
+import Previcon from "@/components/icons/Previcon";
+import Nexticon from "@/components/icons/Nexticon";
+
+import imgProduct1 from "@/assets/images/image-product-1.jpg";
+
+import imgProduct2 from "@/assets/images/image-product-1-thumbnail.jpg";
+import imgProduct3 from "@/assets/images/image-product-2-thumbnail.jpg";
+import imgProduct4 from "@/assets/images/image-product-3-thumbnail.jpg";
+import imgProduct5 from "@/assets/images/image-product-4-thumbnail.jpg";
+
 export default () => {
-    return (
-        <section>
-        Galeria de imagenes
+  return (
+    <section className="grid md:grid-cols-4 gap-4">
+      <div className="col-span-4">
+        <img src={ imgProduct1 } alt="" className="aspect-[16/13]" />
+        <div>
+            <button>
+                <Previcon />
+            </button>
+            <button>
+                <Nexticon />
+            </button>
+        </div>
+      </div>
+      <img src={imgProduct2} alt="" className="hidden md:block" />
+      <img src={imgProduct3} alt="" className="hidden md:block" />
+      <img src={imgProduct4} alt="" className="hidden md:block" />
+      <img src={imgProduct5} alt="" className="hidden md:block" />
     </section>
-    );
+  );
 };
